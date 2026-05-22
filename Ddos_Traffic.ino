6d163267e2d5a1bb47d93152b5ea86c1e6b81392
udayb17 <udaybiswas1712@gmail.com>
2026-05-22 19:38:40 +0530

    Update Ddos_Traffic.ino

--- Ddos_Traffic.ino
+++ Ddos_Traffic.ino
@@ -1,1 +1,44 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
 
+// 🔐 WiFi credentials
+const char* ssid = "OnePlus Nord CE4";
+const char* password = "";
+
+// 🎯 Your Flask server
+const char* server = "http://10.221.202.11:5000";
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(300);
+    Serial.print(".");
+  }
+
+  Serial.println("\n✅ Connected to WiFi");
+}
+
+void loop() {
+
+  if (WiFi.status() == WL_CONNECTED) {
+
+    HTTPClient http;
+
+    // 🔥 Reuse connection for speed
+    http.begin(server);
+
+    int httpCode = http.GET();
+
+    Serial.print("Flooding... Response: ");
+    Serial.println(httpCode);
+
+    http.end();
+  }
+
+  // 🔴 ULTRA FAST REQUESTS = DDoS SIMULATION
+  delay(1);   // ⚠️ very aggressive (can also use delay(0))
+}
